5659e5269ca7192a2d33b3e383a388b4ffc96c60
sola <s.samuel3@yahoo.com>
2022-06-03 17:10:13 +0100

    7, 8, 9

---
+++ ex7.py
@@ -0,0 +1,38 @@
+# this line prints out the string "Mary had a little lamb."
+print("Mary had a little lamb.")
+# this line prints out a string using a format character
+print("Its fleece was white as %s." % 'snow')
+# this line prints out the string "And everywhere that Mary went."
+print("And everywhere that Mary went.")
+# this line prints out 10 "." concatenated as a string
+print("." * 10) # what'd that do?
+
+# this line assigns a character to a variable
+end1 = "C"
+# this line assigns a character to a variable
+end2 = "h"
+# this line assigns a character to a variable
+end3 = "e"
+# this line assigns a character to a variable
+end4 = "e"
+# this line assigns a character to a variable
+end5 = "s"
+# this line assigns a character to a variable
+end6 = "e"
+# this line assigns a character to a variable
+end7 = "B"
+# this line assigns a character to a variable
+end8 = "u"
+# this line assigns a character to a variable
+end9 = "r"
+# this line assigns a character to a variable
+end10 = "g"
+# this line assigns a character to a variable
+end11 = "e"
+# this line assigns a character to a variable
+end12 = "r"
+
+# this line prints a concatenation of the character variable defined above
+# watch that comma at the end. try removing it and see what happens
+print(end1 + end2 + end3 + end4 + end5 + end6,
+end7 + end8 + end9 + end10 + end11 + end12)
