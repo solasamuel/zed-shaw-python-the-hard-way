c78df79992aed6a7ace8b1d47094c41e22e8718d
sola <s.samuel3@yahoo.com>
2022-06-06 17:46:40 +0100

    13, 14, 15

---
+++ ex13.py
@@ -0,0 +1,33 @@
+from sys import argv
+
+# script, first, second, third = argv
+
+# print("The script is called:", script)
+# print("Your first variable is:", first)
+# print("Your second variable is:", second)
+# print("Your third variable is:", third)
+
+# fewer arguments
+# script, first, second = argv
+
+# print("The script is called:", script)
+# print("Your first variable is:", first)
+# print("Your second variable is:", second)
+
+# more arguments
+# script, first, second, third, fourth = argv
+
+# print("The script is called:", script)
+# print("Your first variable is:", first)
+# print("Your second variable is:", second)
+# print("Your third variable is:", third)
+# print("Your fourth variable is:", fourth)
+
+# arguments and user input
+script, first, second = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+third = input("Your third variable is: ")
+fourth = input("Your fourth variable is: ")
---
+++ ex15.py
@@ -0,0 +1,23 @@
+# # This line import the feature argv used to pull argument variables
+# from sys import argv
+
+# # this line pulls those argument variables
+# script, filename = argv
+
+# the line opens a file with the filename argument
+txt = open(filename)
+
+# # this line prints out the file name and its content
+# print("Here's your file %r:" % filename)
+# print(txt.read())
+
+txt.close()
+
+# # this line prompts the user for another filename
+# print("Type the filename again:")
+file_again = input("filename: ")
+
+# these lines open the file and print it's content
+txt_again = open(file_again)
+print(txt_again.read())
+txt_again.close()
